52bc833518b9c3daaccd6c92e3c306146418c85d
Crowdbotics <team@crowdbotics.com>
2021-10-19 19:16:53 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/trening_aplikacija__31562/MainActivity.java
@@ -0,0 +1,15 @@
+package com.trening_aplikacija__31562;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "trening_aplikacija__31562";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "trening_aplikacija__31562",
+  url: "https://trening_aplikacija__31562.botics.co",
+  api: "https://trening_aplikacija__31562.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
